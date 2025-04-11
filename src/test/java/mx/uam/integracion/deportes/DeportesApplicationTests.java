3442f2e36c4c3b539810160cd9a10fe380f082f5
Jonathan Silvestre Alvarez Cruz <silvestrej90@gmail.com>
2025-04-11 10:21:12 -0600

    first commit

---
+++ src/test/java/mx/uam/integracion/deportes/DeportesApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.uam.integracion.deportes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeportesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
